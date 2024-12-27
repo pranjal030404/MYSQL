0b6b41c884b3706ab0a4da8706a8c27e23ca76bc
pranjal030404 <pranjalp160@gmail.com>
2024-12-27 21:45:13 +0530

    comment added 2

---
+++ comment.sql
@@ -0,0 +1,3 @@
+-- Single-line comment: Use the -- symbol followed by a space or a control character to comment out a single line of code.
+-- For example:
+SELECT * FROM employees -- this is a comment
