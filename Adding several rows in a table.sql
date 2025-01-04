6ac1074c6bd04b83382aa777aa309363a7a0e37b
pranjal030404 <pranjalp160@gmail.com>
2025-01-05 00:36:05 +0530

    several rows added

---
+++ Adding several rows in a table.sql
@@ -0,0 +1,5 @@
+--  Adding several rows in a table 
+
+insert into bio_data
+values ("Rishabh",20,"2005-05-21",+91,"rishabh@gmail.com","Purana Bus Adda",21.99), ("ansh",20,"2004-03-17",+91,"ansh@gmail.com","gurgao",19.09),
+("Prakhar",20,"2004-03-21",+91,"prakhar@gmail.com","Padhri Bazar",27.24);
